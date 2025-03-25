c9737c832ee021a70c9c0ec75ba3c5cc2d46c3ea
haylie21 <haylie.antolin08@gmail.com>
2025-03-26 01:05:15 +0800

    Update arrayMethodsActivity.js

--- arrayMethodsActivity.js
+++ arrayMethodsActivity.js
@@ -1,7 +1,7 @@
 // Use concat() to join two arrays  
-let arr1 = ["Cecile", "Lone"];  
-let arr2 = ["Emil", "Tobias", "Linus"];  
-let combinedArr = arr1.concat(arr2);  
+let group1 = ["Cecile", "Lone"];  
+let group2 = ["Emil", "Tobias", "Linus"];  
+let combinedArr = group1.concat(group2);  
 console.log("Combined Array:", combinedArr);  
 
 // Use push() to add an element "Kiwi" to the fruits array  
@@ -9,18 +9,18 @@ let fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.push("Kiwi");  
 console.log("Fruits after push:", fruits);  
 
-// Use unshift() to add two numbers 4 and 5 to the beginning of the array arr3  
-let arr3 = [1, 2, 3];  
-arr3.unshift(4, 5);  
-console.log("Array after unshift:", arr3);  
+// Use unshift() to add two numbers 4 and 5 to the beginning of the array numbers  
+let numbers = [1, 2, 3];  
+numbers.unshift(4, 5);  
+console.log("Numbers after unshift:", numbers);  
 
 // Use pop() to remove the last element from the fruits array  
 fruits.pop();  
 console.log("Fruits after pop:", fruits);  
 
-// Use shift() to remove the first element from the array arr2  
-arr2.shift();  
-console.log("Array 2 after shift:", arr2);  
+// Use shift() to remove the first element from the group2 array  
+group2.shift();  
+console.log("Group2 after shift:", group2);  
 
 // Use sort() to sort the fruits array alphabetically  
 fruits.sort();  
@@ -33,5 +33,5 @@ console.log("Sliced Fruits Array:", slicedFruits);
 // Use splice() to insert and remove elements in the months array  
 let months = ["January", "March", "April", "June"];  
 months.splice(1, 0, "February"); // Insert "February" at index 1  
-months.splice(4, 1, "May"); // Replace at index 4 with "May"  
-console.log("Months Array after splice:", months);  
+months.splice(4, 1, "May"); // Replace "June" with "May"  
+console.log("Months Array after splice:", months);
